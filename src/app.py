a8236665c53a1f98e9de7feea0a65393cf07edc3
Peter Mullins <jc310xc@gmail.com>
2023-06-30 23:35:34 -0400

    leaving a TODO to remove secret key from source...

--- src/app.py
+++ src/app.py
@@ -7,6 +7,7 @@ def create_app():
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:localdev@localhost/festie-db'
+    # TODO: Probably don't have this committed to source control
     app.config['SECRET_KEY'] = "\x8dvts\x8d\r\xe3+\xec\x0cK\x19\xdf,\x11\xfe\xf4\xff\xa40sOG\xf9\x89\x1b\xcb\xc9\xb7\x06\xbcd"
 
     db.init_app(app)
